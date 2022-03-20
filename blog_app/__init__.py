b5d2f2e1d68781994290bbf0e8b553b703207059
adarsh2012 <tadarsh2012@gmail.com>
2022-03-20 23:13:06 +0700

    Added sqlalchemy object

--- blog_app/__init__.py
+++ blog_app/__init__.py
@@ -3,13 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 import flask_login
 
-
+db = SQLAlchemy()
 
 def create_app():
     app = flask.Flask(__name__)
     app.config["SECRET_KEY"] = "adsd"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost:3306/mini_blog"
+    db.init_app(app)
 
-    from .auth import auth
+    from .auth import auth  
     from .blog_view import blog_page
     app.register_blueprint(auth, url_prefix="/auth")
     app.register_blueprint(blog_page, url_prefix="/blog")
