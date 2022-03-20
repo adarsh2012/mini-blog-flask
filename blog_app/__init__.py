67610a84443251a28669e8a9199c2484f5eb9938
adarsh2012 <tadarsh2012@gmail.com>
2022-03-21 00:09:19 +0700

    Added login mangement

--- blog_app/__init__.py
+++ blog_app/__init__.py
@@ -1,15 +1,27 @@
 import flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-import flask_login
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 
 def create_app():
     app = flask.Flask(__name__)
     app.config["SECRET_KEY"] = "adsd"
+                            
+    #DB configuration
     app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost:3306/mini_blog"
+    from .models import User
     db.init_app(app)
+
+    #Login
+    login_manager = LoginManager()
+    login_manager.login_view =  "auth.view"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     #Routing
     from .auth import auth  
