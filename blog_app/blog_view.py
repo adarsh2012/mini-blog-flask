1d4d6a9cd6f1fad201752f473d651bc5c7c2dc83
adarsh2012 <tadarsh2012@gmail.com>
2022-03-21 04:39:01 +0700

    Added function to handle when users request to delete blogs

--- blog_app/blog_view.py
+++ blog_app/blog_view.py
@@ -3,13 +3,23 @@ from urllib import request
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from .models import User, Category, Blog, Status
+from sqlalchemy import desc
 from . import db
 blog_page = Blueprint("blog", __name__)
 
 @login_required
 @blog_page.route("/")
 def home_page():
-    return render_template("home_page.html")
+    blogs = Blog.query.order_by(desc(Blog.date_created)).all()
+    blog_list = []
+    for blog in blogs:
+        username = User.query.filter_by(id=blog.created_by).first().username
+        category_name = Category.query.filter_by(id=blog.category).first().category_name
+        status_name = Status.query.filter_by(id=blog.status).first().status
+        data = {"id" : blog.id, "title" : blog.name, "content" : blog.content, "created_by" : username, 
+                "status" : status_name, "category" : category_name, "date_created" : blog.date_created}
+        blog_list.append(data)
+    return render_template("home_page.html", blogs=blog_list)
 
 @login_required
 @blog_page.route("/create", methods=["GET", "POST"])
@@ -26,3 +36,19 @@ def create_page():
         db.session.commit()
         flash("Created new blog!")
     return render_template("create_blog.html", categories=categories, statuses=statuses)
+
+@login_required
+@blog_page.route("/delete/<id>", methods=["GET", "POST"])
+def delete_post(id):
+    print(id)
+    blog = Blog.query.filter_by(id=id).first()
+    if(blog is None):
+        flash("No such blog exists!", category="error")
+    elif(blog.created_by == current_user.id):
+        # Valid user
+        db.session.delete(blog)
+        db.session.commit()
+        flash("Blog removed successfully!")
+    else:
+        flash("You are not the owner of this blog!", category="error")
+    return redirect(url_for("blog.home_page"))
