747f2ec2b7ce69289c060660896eebcff77f6e2c
adarsh2012 <tadarsh2012@gmail.com>
2022-03-21 03:36:40 +0700

    Added logic to create blog

--- blog_app/blog_view.py
+++ blog_app/blog_view.py
@@ -1,13 +1,17 @@
+from unicodedata import category
 from urllib import request
 from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_required, current_user
 from .models import User, Category, Blog, Status
+from . import db
 blog_page = Blueprint("blog", __name__)
 
-
+@login_required
 @blog_page.route("/")
 def home_page():
     return render_template("home_page.html")
 
+@login_required
 @blog_page.route("/create", methods=["GET", "POST"])
 def create_page():
     categories = Category.query.all()
@@ -17,4 +21,8 @@ def create_page():
         statusId = request.form.get("Status")
         title = request.form.get("title")
         content = request.form.get("content")
+        new_blog = Blog(name=title, status=statusId, category=categoryId, content=content, created_by=current_user.id)
+        db.session.add(new_blog)
+        db.session.commit()
+        flash("Created new blog!")
     return render_template("create_blog.html", categories=categories, statuses=statuses)
